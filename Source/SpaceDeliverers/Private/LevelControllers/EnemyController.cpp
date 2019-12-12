544882d241c3d4cee4c6928056e5c011df07135b
TeeNik <teenik3315@gmail.com>
2019-12-13 02:37:03 +0300

    map

--- Source/SpaceDeliverers/Private/LevelControllers/EnemyController.cpp
+++ Source/SpaceDeliverers/Private/LevelControllers/EnemyController.cpp
@@ -2,6 +2,7 @@
 #include "Engine/World.h"
 #include "SpaceLevelScript.h"
 #include "EnemyShip.h"
+#include "HealthComponent.h"
 
 UEnemyController::UEnemyController()
 {
@@ -29,23 +30,32 @@ void UEnemyController::TickComponent(float DeltaTime, ELevelTick TickType, FActo
 	if (NumberOfEnemies > 0 && seconds > ShootTime)
 	{
 		int index = FMath::RandRange(0, NumberOfEnemies - 1);
-		UE_LOG(LogTemp, Log, TEXT("Index Rand: %d", index));
-		//Ships[index]->Shoot();
+		UE_LOG(LogTemp, Log, TEXT("Index Rand: %d"), index);
+		ShootTime = seconds + ShootRate;
+		Ships[index]->Shoot();
 	}
 	if (NumberOfEnemies < SpawnPoints->Num() && seconds > SpawnTime) 
 	{
 		int index = -1;
 		SpawnInfo.Find(false, index);
 		if (index >= 0) {
-			++NumberOfEnemies;
 			AActor* point = (*SpawnPoints)[index];
 			FVector position = point->GetActorLocation();
 			FRotator rotation = point->GetActorRotation();
 			FActorSpawnParameters ActorSpawnParams;
 			ActorSpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
 			AEnemyShip* enemy = GetWorld()->SpawnActor<AEnemyShip>(EnemyShipBase, position, rotation, ActorSpawnParams);
+			Ships.Add(enemy);
 			SpawnTime = seconds + SpawnRate;
+			enemy->GetHealthComponent()->OnTakeDamage.AddDynamic(this, &[](int health) {
+				
+			});
+			++NumberOfEnemies;
 		}
 	}
 }
 
+void UEnemyController::OnShipTakeDamage(int health)
+{
+
+}
